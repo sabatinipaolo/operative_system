b996d24c92d54020cfafff8e3e22361b1878d3d1
6Bello <giualbamonte06@gmail.com>
2023-04-27 00:55:43 +0200

    refactoring/trovato-bug_gestione-coda

--- general_scheduling.h
+++ general_scheduling.h
@@ -13,9 +13,10 @@ void inizializza(processo processi[], int numeroDiProcessi);
 //inizializzazione funzione per ordinare i processi in base all'istante di arrivo (bubbleSort)
 void ordinamentoProcessi(processo processi, int processiLength);
 //inizializzazione funzione per ordinare i processi in base al tempo di esecuzione (bubbleSort)
-void ordinamentoProcessiPerTempoDiEsecuzione(vector<processo> processi, int processiLength);
+void ordinamentoProcessiPerTempoDiEsecuzione(vector<processo>& processi, int processiLength);
+//inizializzazione funzione per ordinare i processo in base all'id
+void ordinamentoProcessiId(processo processiOrdinId[], processo processi, int numeroDiProcessi);
 //inizializzazione funzione per scrivere i processi con il loro istante di arrivo ed il loro cpuBurst
-void ordinamentoProcessiPerTempoDiEsecuzione(vector<processo> processi, int processiLength);
 void printProcessi(processo processi[], int processiLength);
 //inizializzazione funzione per trovare l'attesa media dei processi
 float trovaAttesaMedia(int attesaCoda[], int processiLength);
@@ -25,6 +26,8 @@ int trovaCompletamento(processo processi[], int attesaCoda[], int processiLength
 float trovaCompletamentoMedia(int completamentoTotale, int processiLength);
 //ritorna il tempo totale che la cpu ha messo per terminare l'esecuzione
 int trovaTempoTotale(processo processi[], int numeroDiProcessi);
+// // Definizione della funzione ordinamentoProcessiId
+// processo *ordinamentoProcessiId(processo processi[], int numeroDiProcessi)
 
 int nProcessi(){
 	int n;
@@ -56,9 +59,12 @@ void ordinamentoProcessi(processo processi[], int processiLength){
 	}
 }
 
-void ordinamentoProcessiPerTempoDiEsecuzione(vector<processo> processi, int processiLength){
+void ordinamentoProcessiPerTempoDiEsecuzione(vector<processo> &processi, int processiLength){
+	for(int i=0; i<processiLength; i++){
+		cout<<processi[i].id;
+	}
 	for(int j=0; j<processiLength; j++){
-		for (int i=0; i<processiLength; i++){
+		for (int i=0; i<processiLength-1; i++){
 			if(processi[i].tempoDiEsecuzione>processi[i+1].tempoDiEsecuzione){
 				processo swap = processi[i];
 				processi[i] = processi[i+1];
@@ -70,6 +76,21 @@ void ordinamentoProcessiPerTempoDiEsecuzione(vector<processo> processi, int proc
 		cout<<processi[i].id;
 	}
 	cout<<endl;
+}
+
+void ordinamentoProcessiId(processo processiOrdinId[] ,processo processi[], int numeroDiProcessi){    
+	for(int i=0; i<numeroDiProcessi; i++){
+		processiOrdinId[i] = processi[i];
+	}
+	for (int j = 0; j < numeroDiProcessi; j++){
+        for (int i = 0; i < numeroDiProcessi; i++){
+            if (processiOrdinId[i].id > processiOrdinId[i + 1].id){
+                processo swap = processiOrdinId[i];
+                processiOrdinId[i] = processiOrdinId[i + 1];
+                processiOrdinId[i + 1] = swap;
+            }
+        }
+    }
 }
 
 void printProcessi(processo processi[], int processiLength){	
@@ -109,19 +130,16 @@ int trovaTempoTotale(processo processi[], int numeroDiProcessi){
 	return tempoTotale;
 }
 
-// Definizione della funzione ordinamentoProcessiId
-// La funzione accetta come argomenti un array di processi e il numero di processi
-// e restituisce un puntatore a un nuovo array di processi ordinato per ID
-processo *ordinamentoProcessiId(processo processi[], int numeroDiProcessi){
-    // Allocazione dinamica della memoria per l'array di processi ordinato
-    processo *processiOrdinId = (processo*) malloc(numeroDiProcessi * sizeof(processo)); //malloc si usa per prendere una porzione di memoria a cui poi si andranno a mettere i valori dell'array, i questo modo quando si ritornano i valori tramite il puntatore questo punta ad una vera cella di memoria perchè quest'ultima non viene svuotata alla fine della funzione! 
+// processo *ordinamentoProcessiId(processo processi[], int numeroDiProcessi){
+//     // Allocazione dinamica della memoria per l'array di processi ordinato
+//     processo *processiOrdinId = (processo*) malloc(numeroDiProcessi * sizeof(processo)); //malloc si usa per prendere una porzione di memoria a cui poi si andranno a mettere i valori dell'array, i questo modo quando si ritornano i valori tramite il puntatore questo punta ad una vera cella di memoria perchè quest'ultima non viene svuotata alla fine della funzione! 
     
-    // Copia dei processi dall'array originale all'array ordinato
-    for (int i = 0; i < numeroDiProcessi; i++)
-    {
-        processiOrdinId[i]=processi[i];
-    }
+//     // Copia dei processi dall'array originale all'array ordinato
+//     for (int i = 0; i < numeroDiProcessi; i++)
+//     {
+//         processiOrdinId[i]=processi[i];
+//     }
     
-    // Restituzione del puntatore all'array di processi ordinato
-	return processiOrdinId;
-}
+//     // Restituzione del puntatore all'array di processi ordinato
+// 	return processiOrdinId;
+// }
