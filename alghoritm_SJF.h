85b0d608d640398f47d004b1ec0978142702ec0e
6Bello <giualbamonte06@gmail.com>
2023-04-30 13:22:55 +0200

    SJF_funzionante-su-vs-no

--- alghoritm_SJF.h
+++ alghoritm_SJF.h
@@ -1,2 +1,29 @@
 #pragma once 
+#include <vector>
 #include "struct_processo.h"
+#include "general_scheduling.h"
+
+void esecuzioneSJF(int numeroDiProcessi, processo processi[], int tempoTotale, int *sequenzaTemporale ){    
+    int prcInArrivo = 0, prcInEsecuzione = processi[0].id;          // numeroCoda serve a tenere il conto nella coda
+    vector<processo> coda;                                          // creo coda con vector per poter aggiungere all'arrivo
+	// processo *processiOrdinId=ordinamentoProcessiId(processi, numeroDiProcessi); //ancora da decidere se usare o no malloc
+	processo processiOrdinId[numeroDiProcessi];
+	ordinamentoProcessiId(processiOrdinId, processi, numeroDiProcessi);
+    for (int i = 0; i < tempoTotale; i++)
+    {
+        if (processi[prcInArrivo].istanteDiArrivo == i)
+        {
+            coda.push_back(processi[prcInArrivo]); // quando un processo arriva lo aggiungo alla coda
+            prcInArrivo++;                         // aggiungo uno ai prcInArrivo in modo da controllare quando arriva il processo successivo
+        }
+        if (processiOrdinId[prcInEsecuzione].tempoRimanente == 0)
+        {
+            coda.erase(coda.begin());                                   // quando un processo finisce lo elimino dalla coda per evitare problemi di ordinamento
+            ordinamentoProcessiPerTempoDiEsecuzione(coda, coda.size()); // li riordino
+            cout << "esce " << prcInEsecuzione << " a " << i << " ed entra " << coda[0].id << endl;
+            prcInEsecuzione = coda[0].id; // il processo in esecuzione diventa il primo della coda
+        }
+        processiOrdinId[prcInEsecuzione].tempoRimanente--; // il tempo rimanente per il processo in esecuzione diminuisce di uno
+        sequenzaTemporale[i] = prcInEsecuzione;     // durante quell'istante c'è quel processo in esecuzione e do quel dato in modo da poterlo stampare dopo
+    }
+}
--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 #include <vector>
 #include "general_scheduling.h"
 #include "signature.h"
-
+#include "alghoritm_SJF.h"
 using namespace std;
 
 // MAIN
@@ -18,36 +18,14 @@ int main()
 {
     // int numeroDiProcessi=nProcessi();
     int numeroDiProcessi = 5;
-    processo processi[numeroDiProcessi] = {{0, 2, 2, 2}, {1, 0, 1, 1}, {2, 1, 3, 3}, {3, 3, 2, 2}, {4, 4, 1, 1}};
+    processo processi[numeroDiProcessi] = {{0, 2, 3, 3}, {1, 0, 2, 2}, {2, 1, 3, 3}, {3, 3, 2, 2}, {4, 4, 1, 1}};
     // svolgiConFCFSNonPreventive(processi, numeroDiProcessi);
     // inizializza(processi,numeroDiProcessi);
     ordinamentoProcessi(processi, numeroDiProcessi); // ordino i processi per istante di arrivo
     // printProcessi(processi, numeroDiProcessi);
     int tempoTotale = trovaTempoTotale(processi, numeroDiProcessi); // tempo totale per il completamento dei processi
-    int prcInArrivo = 0, prcInEsecuzione = processi[0].id;          // numeroCoda serve a tenere il conto nella coda
-    vector<processo> coda;                                          // creo coda con vector per poter aggiungere all'arrivo
     int sequenzaTemporale[tempoTotale];
-	// processo *processiOrdinId=ordinamentoProcessiId(processi, numeroDiProcessi); //ancora da decidere se usare o no malloc
-	processo processiOrdinId[numeroDiProcessi];
-	ordinamentoProcessiId(processiOrdinId, processi, numeroDiProcessi);
-
-    for (int i = 0; i < tempoTotale; i++)
-    {
-        if (processi[prcInArrivo].istanteDiArrivo == i)
-        {
-            coda.push_back(processi[prcInArrivo]); // quando un processo arriva lo aggiungo alla coda
-            prcInArrivo++;                         // aggiungo uno ai prcInArrivo in modo da controllare quando arriva il processo successivo
-        }
-        if (processiOrdinId[prcInEsecuzione].tempoRimanente == 0)
-        {
-            coda.erase(coda.begin());                                   // quando un processo finisce lo elimino dalla coda per evitare problemi di ordinamento
-            ordinamentoProcessiPerTempoDiEsecuzione(coda, coda.size()); // li riordino
-            cout << "esce " << prcInEsecuzione << " a " << i << " ed entra " << coda[0].id << endl;
-            prcInEsecuzione = coda[0].id; // il processo in esecuzione diventa il primo della coda
-        }
-        processiOrdinId[prcInEsecuzione].tempoRimanente--; // il tempo rimanente per il processo in esecuzione diminuisce di uno
-        sequenzaTemporale[i] = prcInEsecuzione;     // durante quell'istante c'è quel processo in esecuzione e do quel dato in modo da poterlo stampare dopo
-    }
+    esecuzioneSJF(numeroDiProcessi, processi, tempoTotale, sequenzaTemporale);
     for (int i = 0; i < tempoTotale; i++)
         cout << "|" << sequenzaTemporale[i] << "|";
     cout << endl;
