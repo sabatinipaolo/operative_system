6ced29a5633529cea293f7bae9e1bf14ba43f8a7
sabatinipaolo <sabatinipaolo@foo.mail>
2023-05-07 19:28:03 +0200

    Refactoring : formattato main.cpp ) con format di VCode

--- main.cpp
+++ main.cpp
@@ -16,17 +16,16 @@ using namespace std;
 // MAIN
 int main()
 {
-    // int numeroDiProcessi=nProcessi();
-    int numeroDiProcessi = 5;
-    processo processi[numeroDiProcessi] = {{0, 2, 4, 4}, {1, 0, 6, 6}, {2, 1, 1, 1}, {3, 3, 4, 4}, {4, 4, 1, 1}}; 
-    inizializza(processi,numeroDiProcessi);
-    //ordinamentoProcessi(processi, numeroDiProcessi); // ordino i processi per istante di arrivo
-    // printProcessi(processi, numeroDiProcessi);
-    int tempoTotale ;
-    int sequenzaTemporale[100]; //TODO : transitorio fino al prossimo refactoring 
-    esecuzioneSJFPrentive(numeroDiProcessi, processi, tempoTotale, sequenzaTemporale);
-    stampaSequenzaTemporale( tempoTotale, sequenzaTemporale);
-    // signatureWalterWhite();
 
+    int numeroDiProcessi = 5;
+    processo processi[numeroDiProcessi] = {{0, 2, 4, 4}, {1, 0, 6, 6}, {2, 1, 1, 1}, {3, 3, 4, 4}, {4, 4, 1, 1}};
+    inizializza(processi, numeroDiProcessi);
+
+    int tempoTotale;
+    int sequenzaTemporale[100]; // TODO : transitorio fino al prossimo refactoring
+    esecuzioneSJFPrentive(numeroDiProcessi, processi, tempoTotale, sequenzaTemporale);
+    stampaSequenzaTemporale(tempoTotale, sequenzaTemporale);
+
+    // signatureWalterWhite();
     return 0;
 }
